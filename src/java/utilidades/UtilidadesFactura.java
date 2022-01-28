9bb593f8115f0a15504f6bdeee45e275b09d92a6
Biasem <rnietourena@safareyes.es>
2022-01-28 20:30:48 +0100

    comienzo de metodos

---
+++ src/java/utilidades/UtilidadesFactura.java
@@ -0,0 +1,28 @@
+package utilidades;
+
+import modelos.Factura;
+import modelos.LineaFactura;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class UtilidadesFactura {
+
+    public boolean esFacturaVencida(Factura factura){
+
+        if(factura.getFechaVencimiento().isAfter(LocalDate.now())){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public double calcularBaseFactura(Factura factura){
+        factura.getLineaFactura();
+        for (){
+
+        }
+
+
+    }
+
+}
--- src/main/java/modelos/Producto.java
+++ src/main/java/modelos/Producto.java
@@ -10,53 +10,72 @@ public class Producto {
     private LocalDate fechaCaducidad;
     private TipoProducto tproductos;
     private Almacen almacen;
+    private double precio;
+
+    public Producto(int identificador, String codigo, String descripcion, LocalDate fechaCaducidad, TipoProducto tproductos, Almacen almacen, double precio) {
+        this.identificador = identificador;
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+        this.fechaCaducidad = fechaCaducidad;
+        this.tproductos = tproductos;
+        this.almacen = almacen;
+        this.precio = precio;
+    }
 
     public int getIdentificador() {
         return identificador;
-    }
-
-    public String getCodigo() {
-        return codigo;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public LocalDate getFechaCaducidad() {
-        return fechaCaducidad;
-    }
-
-    public TipoProducto getTproductos() {
-        return tproductos;
-    }
-
-    public Almacen getAlmacen() {
-        return almacen;
     }
 
     public void setIdentificador(int identificador) {
         this.identificador = identificador;
     }
 
+    public String getCodigo() {
+        return codigo;
+    }
+
     public void setCodigo(String codigo) {
         this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
     }
 
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
     }
 
+    public LocalDate getFechaCaducidad() {
+        return fechaCaducidad;
+    }
+
     public void setFechaCaducidad(LocalDate fechaCaducidad) {
         this.fechaCaducidad = fechaCaducidad;
+    }
+
+    public TipoProducto getTproductos() {
+        return tproductos;
     }
 
     public void setTproductos(TipoProducto tproductos) {
         this.tproductos = tproductos;
     }
 
+    public Almacen getAlmacen() {
+        return almacen;
+    }
+
     public void setAlmacen(Almacen almacen) {
         this.almacen = almacen;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
     }
 
     @Override
