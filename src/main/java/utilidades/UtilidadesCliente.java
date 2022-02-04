5b74e44c2981825a5a51dd7fef19b6733285ff53
lortegaperal <lortegaperal@safareyes.es>
2022-02-04 21:00:22 +0100

    Ej 6 parte 2 proyecto

--- src/main/java/utilidades/UtilidadesCliente.java
+++ src/main/java/utilidades/UtilidadesCliente.java
@@ -1,4 +1,17 @@
 package utilidades;
 
+import modelos.Cliente;
+
+import javax.swing.text.StringContent;
+
 public class UtilidadesCliente {
+    public boolean esDniValido(Cliente cliente){
+        if (cliente.getDni().length() == 9 && cliente.getDni().charAt(9) ){
+            return true;
+
+        }else {
+            return false;
+        }
+    }
+
 }
