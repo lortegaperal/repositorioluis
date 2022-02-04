ee90d9c0d81ad16b9d7074887c1c6d3c7121de0f
lortegaperal <lortegaperal@safareyes.es>
2022-02-04 20:17:01 +0100

    Ej 5 parte 2 proyecto

--- src/main/java/utilidades/UtilidadesProducto.java
+++ src/main/java/utilidades/UtilidadesProducto.java
@@ -9,31 +9,21 @@ import java.util.List;
 public class UtilidadesProducto {
 
     public List<Producto> getPorTipo(List<Producto> productos, TipoProducto tipo) {
+        for (Producto producto : productos) {
+            if (producto.getTproductos().equals(tipo)) {
+                productos.add(producto);
+            }
+        }
 
         return productos;
-
-    }
-    public List<Producto> getPorAlmacen(List<Producto> productos, Almacen almacen){
-
-//    public static List<Coche> obtenerCochesDelPais(List<Coche> listaCoches, String pais){
-//        List<Coche> listaCochesPais = new ArrayList<>();
-//
-//        for(Coche coche : listaCoches){
-//
-//            //Compruebo si el coche que estoy recorriendo es del mismo pais del que me pasan
-//            if(coche.getMarca().getPais().equals(pais)){
-//
-//                //Añadimos el coche a la lista que vamos a devolver
-//                listaCochesPais.add(coche);
-//            }
-//        }
-//        return listaCochesPais;
-//
-//    }
-
     }
 
-
-
-
+    public List<Producto> getPorAlmacen(List<Producto> productos,Almacen almacen){
+        for (Producto producto : productos) {
+            if (producto.getAlmacen().equals(almacen)) {
+                productos.add(producto);
+            }
+        }
+        return productos;
+    }
 }
