c96c2522e4ab26058ae4408baa3294c271a54987
lortegaperal <lortegaperal@safareyes.es>
2022-02-06 15:13:46 +0100

    terminamos el el proyecto parte 2

--- src/main/java/utilidades/UtilidadesCliente.java
+++ src/main/java/utilidades/UtilidadesCliente.java
@@ -5,13 +5,14 @@ import modelos.Cliente;
 import javax.swing.text.StringContent;
 
 public class UtilidadesCliente {
-    public boolean esDniValido(Cliente cliente){
-        if (cliente.getDni().length() == 9 && cliente.getDni().charAt(9) ){
+
+    public static boolean esDniValido(Cliente cliente){
+
+        if (cliente.getDni().length() == 9 && cliente.getDni().matches("^[0-9]{8}[A-Z]{1}$")){
             return true;
 
         }else {
             return false;
         }
     }
-
 }
